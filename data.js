5de6712a85f96e0ba2ef0a0426889a14fd68956b
Linan <linan@umeng.com>
2013-06-18 15:05:23 +0800

    add sth

--- data.js
+++ data.js
@@ -87,10 +87,58 @@ function Ctrl($scope) {
           "description":""
       },
       {
+          "date":"1998.2",
+          "name":"XML",
+          "description":"XML是從1995年開始有其雛形，並向W3C（全球資訊網聯盟）提案，而在1998二月發佈為W3C的標準（XML1.0）。XML的前身是SGML（The Standard Generalized Markup Language），是自IBM從1960年代就開始發展的GML（Generalized Markup Language）標準化後的名稱。"
+      },
+      {
+          "date":"1993.6",
+          "name":"HTML",
+          "description":"HyperText Markup Language"
+      },
+      {
+          "date":"1996.12",
+          "name":"CSS",
+          "description":"於1994年，哈坤·利和伯特·波斯合作設計CSS。他們在1994年首次在芝加哥的一次会议上第一次展示了CSS的建议。"
+      },
+      {
+          "date":"2011.6",
+          "name":"CSS3",
+          "description":""
+      },
+      {
+          "date":"1996.11",
+          "name":"FLASH",
+          "description":""
+      },
+      {
+          "date":"2008.9",
+          "name":"Google Chrome",
+          "description":"Google官方網誌在2008年9月2日撰文說，將於第二天在超過100個國家同時發布「Google Chrome」的測試版。 同日，Google官方網誌宣布「Google Chrome」測試版已經開放下載。"
+      },
+      {
+          "date":"2008.1",
+          "name":"HTML5",
+          "description":"HTML5草案的前身名为Web Applications 1.0，是在2004年由WHATWG提出，再于2007年获W3C接納，并成立了新的HTML工作团队。在2008年1月22日，第一份正式草案发布。"
+      },
+      {
+          "date":"1996",
+          "name":"AJAX",
+          "description":"Internet Explorer将iframe元素加入到HTML，支持局部刷新网页。"
+      },
+      {
+          "date":"2005.7",
+          "name":"JSONP",
+          "description":""
+      }
+/*
+      ,
+      {
           "date":"",
           "name":"",
           "description":""
-      }
+      }*/
+
   ]
       
        
